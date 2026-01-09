675fe279014b681af5c1634124282deab52b1e64
hhhaoi <253811333+hhhaoi@users.noreply.github.com>
2026-01-08 19:19:22 -0500

    update files preventing successful build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-deploy/", // this would be just '/' if it were on a domain
   plugins: [react()],
 })
