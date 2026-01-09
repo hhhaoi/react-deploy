aeae8113b4f905855570c7fb02bb5298e346e9ad
hhhaoi <253811333+hhhaoi@users.noreply.github.com>
2026-01-08 19:27:20 -0500

    add buttons to link between pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import "./home.css";
+import { Link } from 'react-router';
 
 function About() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Link to="/">
+        <button>
+            &lt; back to home
+        </button>
+      </Link>
       <h1>🔥🔥🔥🔥🔥🔥</h1>
       <h1>ABOUT PAGE</h1>
       <div className="card">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import "./home.css";
+import { Link } from 'react-router-dom';
 
 function Home() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Link to="/about">
+        <button>
+            check about page
+        </button>
+      </Link>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
